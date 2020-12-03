36eb03c5cd4bd5b6b8836afdf6e3108ccff3c973
vaishali-vcs <vaishali.bhat@yahoo.com>
2020-12-03 07:16:00 -0500

    initial

--- main_type1.py
+++ main_type1.py
@@ -108,26 +108,6 @@ class DQN:
 
         return loss
 
-    def train_onend(self, TargetNet):
-
-        ids = np.random.randint(low=0, high=len(self.experience['s']), size=self.batch_size)
-        states = np.asarray([self.experience['s'][i] for i in ids])
-        actions = np.asarray([self.experience['a'][i] for i in ids])
-        rewards = np.asarray([self.experience['r'][i] for i in ids])
-        states_next = np.asarray([self.experience['s2'][i] for i in ids])
-        dones = np.asarray([self.experience['done'][i] for i in ids])
-        value_next = np.max(TargetNet.predict(states_next), axis=-1)
-        actual_values = np.where(dones, rewards, rewards + self.gamma * value_next.squeeze())
-
-        with tf.GradientTape() as tape:
-            selected_action_values = tf.math.reduce_sum(
-                self.predict(states) * tf.one_hot(actions, self.num_actions), axis=-1)
-            loss = tf.math.reduce_mean(tf.square(actual_values - selected_action_values))
-        variables = self.model.trainable_variables
-        gradients = tape.gradient(loss, variables)
-        self.optimizer.apply_gradients(zip(gradients, variables))
-        return loss
-
 l1 = 72
 l2 = 150
 l3 = 100
@@ -306,9 +286,9 @@ def playgame():
         if n % 100 == 0:
                 print("episode:", n, "episode reward:", total_reward, "eps:", epsilon, "avg reward (last 100):", avg_rewards,
                       "episode loss: ", losses)
-        print("avg reward for last 100 episodes:", avg_rewards)
+                print("avg reward for last 100 episodes:", avg_rewards)
 
-        print("gameswon=", sumgameswon)
+    print("gameswon=", sumgameswon)
 
     # test(TrainNet, epsilon)
 
